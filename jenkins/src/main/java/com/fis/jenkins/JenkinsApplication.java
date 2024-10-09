611a79434b9fcc99054b57f05d2c4fa50491a50a
Anam Bagwan <65122751+Anambagwan@users.noreply.github.com>
2024-10-09 23:50:49 +0530

    Add files via upload

---
+++ jenkins/src/main/java/com/fis/jenkins/JenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.fis.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	 public String getMessage() {
+	 return "Welcome to FIS Spring boot integration with Jenkins App";
+	 }
+}
---
+++ jenkins/src/test/java/com/fis/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
